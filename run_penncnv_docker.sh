0262ea04837462dddede452e333caaf71f7b006d
lirialmodobar <94066322+lirialmodobar@users.noreply.github.com>
2023-07-14 14:29:07 -0300

    Update run_penncnv_docker.sh

--- run_penncnv_docker.sh
+++ run_penncnv_docker.sh
@@ -5,5 +5,11 @@ chmod 777 $WD
 Rscript $WD/install_necessary_packages.R
 cd $WD/penncnv_pipeline
 bash CNV_detection.sh Config_default.txt
-cd $WD/output/R
-Rscript $WD/save_qs_dataframe.R
+mv $WD/save_qs_dataframe.R $WD/output_teste/R
+cd $WD/output_teste/R
+Rscript $WD/output_teste/R/save_qs_dataframe.R
+mv $WD/save_qs_hist.R $WD/output_teste/R
+cd $WD/output_teste/R
+Rscript $WD/output_teste/R/save_qs_hist.R
+mv $WD/output_teste/R/save_qs_dataframe.R $WD
+mv $WD/output_teste/R/save_qs_hist.R $WD
