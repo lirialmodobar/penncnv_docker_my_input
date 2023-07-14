a2304741d68f5137e332156ff2de71219c17060e
lirialmodobar <94066322+lirialmodobar@users.noreply.github.com>
2023-07-14 15:15:49 -0300

    Update process_input_run_docker.sh

--- process_input_run_docker.sh
+++ process_input_run_docker.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-WD=/mnt/genetica_1/liriel/juliathon/penncnv_docker_my_input #/path/to/cloned/repo
+WD=/mnt/genetica_1/JuliaArendt/liri_juliathon/penncnv_docker_my_input #/path/to/cloned/repo
 [ ! -d "$WD/input" ] && mkdir "$WD/input"
 [ ! -d "$WD/output" ] && mkdir "$WD/output"
-cp /mnt/genetica_1/liriel/juliathon/julia_data/* $WD/input #replace with whatever operation you need to put the samples in the input dir
-mv /mnt/genetica_1/liriel/juliathon/INPD_c.pfb $WD #replace with whatever operation you need to put the pfb file in the WD dir
+cp /mnt/genetica_1/JuliaArendt/liri_juliathon/julia_data/* $WD/input #replace with whatever operation you need to put the samples in the input dir
+mv /mnt/genetica_1/JuliaArendt/liri_juliathon/INPD_c.pfb $WD #replace with whatever operation you need to put the pfb file in the WD dir
 ls $WD/input > $WD/samples.txt
 while read sample; do
        cut -f 1,4,5 $WD/input/$sample > $WD/input/"$sample"_processed
