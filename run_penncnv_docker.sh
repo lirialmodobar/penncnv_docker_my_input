2ab41aa5ef6cbe2183e25be1282212ef9ed47064
lirialmodobar <94066322+lirialmodobar@users.noreply.github.com>
2023-07-14 14:42:38 -0300

    Update run_penncnv_docker.sh

--- run_penncnv_docker.sh
+++ run_penncnv_docker.sh
@@ -8,8 +8,8 @@ bash CNV_detection.sh Config_default.txt
 mv $WD/save_qs_dataframe.R $WD/output/R
 cd $WD/output/R
 Rscript $WD/output/R/save_qs_dataframe.R
-mv $WD/save_qs_hist.R $WD/output #in normal circunstances, this should be $WD/output/R, I didn't complete the pipeline, so my file wasn't moved from output to output/R, so I used it like this, make sure to change it
-cd $WD/output  #in normal circunstances, this should be $WD/output/R, I didn't complete the pipeline, so my file wasn't moved from output to output/R, so I used it like this, make sure to change it
-Rscript $WD/output/save_qs_hist.R  #in normal circunstances, this should be $WD/output/R, I didn't complete the pipeline, so my file wasn't moved from output to output/R, so I used it like this, make sure to change it
+mv $WD/save_qs_hist.R $WD/output #in normal circumstances, this should be $WD/output/R, I didn't complete the pipeline, so my file wasn't moved from output to output/R, so I used it like this, make sure to change it
+cd $WD/output  #in normal circunstamces, this should be $WD/output/R, I didn't complete the pipeline, so my file wasn't moved from output to output/R, so I used it like this, make sure to change it
+Rscript $WD/output/save_qs_hist.R  #in normal circumstances, this should be $WD/output/R/save_qs_hist.R, I didn't complete the pipeline, so my file wasn't moved from output to output/R, so I used it like this, make sure to change it
 mv $WD/output/R/save_qs_dataframe.R $WD
-mv $WD/output/R/save_qs_hist.R $WD
+mv $WD/output/save_qs_hist.R $WD  #in normal circumstances, this should be $WD/output/R/save_qs_hist.R, I didn't complete the pipeline, so my file wasn't moved from output to output/R, so I used it like this, make sure to change it
