767fb0411560d12ce5f7ca51030e179586747314
lirialmodobar <94066322+lirialmodobar@users.noreply.github.com>
2023-07-14 00:31:26 -0300

    Update install_necessary_packages.R

--- install_necessary_packages.R
+++ install_necessary_packages.R
@@ -1,2 +1,2 @@
 install.packages("plyr", dependencies = TRUE, repos = "https://vps.fmvz.usp.br/CRAN/")
-install.packages("string", dependencies = TRUE, repos = "https://vps.fmvz.usp.br/CRAN/")
+install.packages("stringr", dependencies = TRUE, repos = "https://vps.fmvz.usp.br/CRAN/")
