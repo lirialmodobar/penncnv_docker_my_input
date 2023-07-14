58bbd2065f30af9d83b20cc1e3c604322db1941d
lirialmodobar <94066322+lirialmodobar@users.noreply.github.com>
2023-07-14 14:52:01 -0300

    Update run_penncnv_docker.sh

--- run_penncnv_docker.sh
+++ run_penncnv_docker.sh
@@ -8,9 +8,4 @@ bash CNV_detection.sh Config_default.txt
 mv $WD/save_qs_dataframe.R $WD/output/R
 cd $WD/output/R
 Rscript $WD/output/R/save_qs_dataframe.R
-mv $WD/save_qs_hist.R $WD/output #in normal circumstances, this should be $WD/output/R, I didn't complete the pipeline, so my file wasn't moved from output to output/R, so I used it like this, make sure to change it
-cd $WD/output  #in normal circunstamces, this should be $WD/output/R, I didn't complete the pipeline, so my file wasn't moved from output to output/R, so I used it like this, make sure to change it
-Rscript $WD/output/save_qs_hist.R  #in normal circumstances, this should be $WD/output/R/save_qs_hist.R, I didn't complete the pipeline, so my file wasn't moved from output to output/R, so I used it like this, make sure to change it
-mv $WD/output/hist_qs.pdf $WD/output/R #in normal circumstances it already would be on this dir, so you don't need this line if you ran the complete pipeline
 mv $WD/output/R/save_qs_dataframe.R $WD
-mv $WD/output/save_qs_hist.R $WD  #in normal circumstances, this should be $WD/output/R/save_qs_hist.R, I didn't complete the pipeline, so my file wasn't moved from output to output/R, so I used it like this, make sure to change it
